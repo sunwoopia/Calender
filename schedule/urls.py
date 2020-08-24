ab396b9f11fd9c2d3a4a725e8b32e5f0025527c4
김선우 <sunwoopia@kookmin.ac.kr>
2020-08-24 18:55:08 +0900

    django 로그인 및 회원가입 페이지 생성

--- schedule/urls.py
+++ schedule/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('schedule/', views.schedule, name='schedule')
+    path('schedule/', views.schedule, name='schedule'),
+    path('login/', views.login, name='login'),
+    path('signup/', views.signup, name='signup'),
 ]
--- schedule/views.py
+++ schedule/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
-from django.http import HttpResponse
 from .form import postSchedule
-from .models import schedule
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login as auth_login
 
 def index(request):
     return render(request, 'index.html')
@@ -10,7 +10,6 @@ def index(request):
 def schedule(request):
     if request.method == 'POST':
         form = postSchedule(request.POST)
-        print("데이터가 넘어옴")
         if form.is_valid():
             post = form.save(commit=False)
             post.scheduleOwner = request.user.id
@@ -20,3 +19,31 @@ def schedule(request):
     form = postSchedule()
 
     return render(request, 'schedule.html', {'form': form})
+
+def signup(request):
+    # HTTP Method가 POST 인 경우
+    if request.method == 'POST':
+        signup_form = UserCreationForm(request.POST)
+    # 모델폼의 유효성 검증이 valid할 경우, DB에 저장
+        if signup_form.is_valid():
+            signup_form.save()
+        return redirect('/')
+
+# HTTP Method가 GET 인 경우
+    else:
+        signup_form = UserCreationForm()
+
+    return render(request, 'signup.html', {'signup_form': signup_form})
+
+
+def login(request):
+    if request.method == 'POST':
+        login_form = AuthenticationForm(request, request.POST)
+        if login_form.is_valid():
+            auth_login(request, login_form.get_user())
+        return redirect('/')
+
+    else:
+        login_form = AuthenticationForm()
+
+    return render(request, 'login.html', {'login_form': login_form})
